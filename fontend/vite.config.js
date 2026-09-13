8df67e71d6c61139c22a58491c9537330ad03b1c
Sharjeel Ahmed <sharjlbalouch319@gmail.com>
2026-09-13 14:05:10 +0500

    Production issue

--- fontend/vite.config.js
+++ fontend/vite.config.js
@@ -12,7 +12,7 @@ export default defineConfig(({ mode }) => {
       port: 3000,
       proxy: {
         '/api': {
-          target: env.VITE_BACKEND_URL || 'http://localhost:5000',
+          target: env.VITE_BACKEND_URL,
           changeOrigin: true,
         },
       },
