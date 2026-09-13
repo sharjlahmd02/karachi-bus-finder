8cd00713b07c3f26458174abb5bedc2e6a6933af
Sharjeel Ahmed <sharjlbalouch319@gmail.com>
2026-09-13 13:54:31 +0500

    Backend URL issue resolved for production mode

--- fontend/vite.config.js
+++ fontend/vite.config.js
@@ -1,16 +1,21 @@
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-import { defineConfig } from 'vite'
 
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  server: {
-    port: 3000,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:5000',
-        changeOrigin: true
-      }
-    }
+export default defineConfig(({ mode }) => {
+  // Load env variables based on current mode 
+  const env = loadEnv(mode, process.cwd(), '')
+
+  return {
+    plugins: [react(), tailwindcss()],
+    server: {
+      port: 3000,
+      proxy: {
+        '/api': {
+          target: env.VITE_BACKEND_URL || 'http://localhost:5000',
+          changeOrigin: true,
+        },
+      },
+    },
   }
 })
